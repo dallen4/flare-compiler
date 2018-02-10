345744cf1a024a85900b02a98d051e121e1189b9
Nieky Allen <dallen4@luc.edu>
2018-02-10 13:42:49 -0600

    added TODOs in tokenizer file for all current token types, implemented token creation for number and toss whitespace.

--- src/parts/tokenizer.js
+++ src/parts/tokenizer.js
@@ -20,7 +20,50 @@ module.exports = function tokenizer(input = '') {
     // -- property lookup is resource/time intensive task and will save time
     let inputLength = input.length;
 
-    while (current < inputLength);
+    while (current < inputLength) {
+
+        let char = input[current];
+
+        // skip all whitespace
+        if (WHITESPACE.test(char)) {
+            current++;
+            continue;
+        }
+
+        // 'number' token
+        // grab entire number sequence
+        if (NUMBERS.test(char)) {
+
+            // init string to store num terms
+            let value = '';
+
+            // while char is num, add to value
+            // increment after adding using ++current
+            while (NUMBERS.test(char)) {
+                value += char;
+                char = input[++current];
+            }
+
+            // push token with whole num value to tokens
+            tokens.push({ type: 'number', value });
+
+            continue;
+
+        }
+
+        // TODO 'word' token
+
+        // TODO 'string' token
+
+        // TODO 'brace' token
+
+        // TODO 'paren' token
+
+        // TODO 'colon' token
+
+        // TODO 'semi' token
+
+    }
 
     return tokens;
 
