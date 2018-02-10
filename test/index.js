bb732e452bd50f38e4e67229065e8bd5776cfb76
Nieky Allen <dallen4@luc.edu>
2018-02-10 00:23:31 -0600

    tested mocha with dumby unit test.

---
+++ test/index.js
@@ -0,0 +1,11 @@
+// unit tests (mocha)
+const assert = require('assert');
+
+// dumby test from mocha site
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
