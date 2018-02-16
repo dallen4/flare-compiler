5c0a4a6d242cb081c51102a90fdf7126f4bf4fb3
Nieky Allen <dallen4@luc.edu>
2018-02-16 17:07:25 -0600

    completed rough versions of token adding for strings, semicolons, colons, and braces

--- src/parts/tokenizer.js
+++ src/parts/tokenizer.js
@@ -47,7 +47,6 @@ module.exports = function tokenizer(input = '') {
 
                 // push token with word value to tokens
                 tokens.push({ type: 'word', value });
-
                 continue;
 
             // 'number' token
@@ -63,28 +62,107 @@ module.exports = function tokenizer(input = '') {
 
                 // push token with num value to tokens
                 tokens.push({ type: 'number', value });
+                continue;
+            
+            // 'string' token
+            case char === '\'':
+                
+                // skip quote
+                char = input[++current];
 
+                // while char isn't end quote, add to value
+                // increment after adding using ++current
+                while (char !== '\'') {
+                    value += char;
+                    char = input[++current];
+                }
+
+                // skip quote
+                char = input[++current];
+
+                // push token with string value to tokens
+                tokens.push({ type: 'string', value });
+                continue;
+            
+            // "string" token
+            case char === '"':
+                
+                // skip quote
+                char = input[++current];
+
+                // while char isn't end quote, add to value
+                // increment after adding using ++current
+                while (char !== '"') {
+                    value += char;
+                    char = input[++current];
+                }
+
+                // skip quote
+                char = input[++current];
+
+                // push token with string value to tokens
+                tokens.push({ type: 'string', value });
+                continue;
+
+            // open 'paren' token
+            case char === '(':
+                tokens.push({
+                    type: 'paren',
+                    value: char
+                });
+                current++;
+                continue;
+            
+            // close 'paren' token
+            case char === ')':
+                tokens.push({
+                    type: 'paren',
+                    value: char
+                });
+                current++;
+                continue;
+
+            // 'semi' token
+            case char === ';':
+                tokens.push({
+                    type: 'semi',
+                    value: char
+                });
+                current++;
+                continue;
+            
+            // 'colon' token
+            case char === ':':
+                tokens.push({
+                    type: 'colon',
+                    value: char
+                });
+                current++;
+                continue;
+
+            // open 'brace' token
+            case char === '{':
+                tokens.push({
+                    type: 'brace',
+                    value: char
+                });
+                current++;
+                continue;
+            
+            // close 'brace' token
+            case char === '}':
+                tokens.push({
+                    type: 'brace',
+                    value: char
+                });
+                current++;
                 continue;
             
             // unknown token type
             // -- throw TypeError
             default:
-                throw new TypeError('Unexpected token: ' + char);
-
-            // TODO 'string' token
-            case char === '\'' || char === '"':
-
-            // TODO 'brace' token
-            case char === '{' || char === '}':
-
-            // TODO 'paren' token
-            case char === '(' || char === ')':
-
-            // TODO 'colon' token
-            case char === ':':
-
-            // TODO 'semi' token
-            case char === ';':
+                throw new TypeError('Unexpected token: ' + char
+                    + '\nPlease see documentation for syntax guidelines.');
             
         }
 
