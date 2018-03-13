7953a7da04d63737ea923445b6445915afcee528
Nieky Allen <dallen4@luc.edu>
2018-03-13 12:42:04 -0500

    added notes about visitor object structure/implementation

--- src/parts/transformer/index.js
+++ src/parts/transformer/index.js
@@ -4,6 +4,8 @@ const traverser = require('./traverser');
 'use strict';
 
 // TODO define visitor object to pass to Traverser
+// visitor object should include:
+// -- function to filter document nodes out of collection nodes
 const visitor = {};
 
 module.exports = function transformer(ast = {}) {
