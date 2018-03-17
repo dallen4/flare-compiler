0405000885d793d163a8930d59dc3ea3f38daf48
Nieky Allen <dallen4@luc.edu>
2018-03-16 21:17:51 -0500

    implemented tests for tokenizer and parser using sample input from data.js -- both tests passing

--- test/index.js
+++ test/index.js
@@ -1,11 +1,33 @@
 // unit tests (mocha)
 const assert = require('assert');
 
-// dumby test from mocha site
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+// import compiler functions
+var {
+    tokenizer,
+    parser
+} = require('../src/parts');
+
+// import test data
+var {
+    voltCode,
+    tokens,
+    ast
+} = require('./data');
+
+describe('Volt Compiler', function() {
+
+    describe('Tokenizer', function() {
+        it('should generate correct array of tokens from volt code', function() {
+            var testTokens = tokenizer(voltCode);
+            assert.deepStrictEqual(tokens, testTokens);
+        });
     });
-  });
+    
+    describe('Parser', function() {
+        it('should generate correct AST from tokens array', function() {
+            var testAst = parser(tokens);
+            assert.deepStrictEqual(ast, testAst);
+        });
+    });
+    
 });
